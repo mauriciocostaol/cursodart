26e427abb824bccc265bdc2402fa01a37d88635e
Maurício Ricardo <43684435+mauriciocostaol@users.noreply.github.com>
2023-09-25 16:14:12 -0300

    Aulas 1 e 2

---
+++ lib/primeiroprojeto.dart
@@ -0,0 +1,10 @@
+void main() {
+  int numero = 0X2FA;
+  double altura = 780e6;
+  bool option = true;
+  print(option);
+
+  List<String> names = ['Wesley','Yuri','Guedes'];
+
+  print(names.length);
+}
